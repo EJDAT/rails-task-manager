1a13c2fd89776d57d3ac3ef090daac3db7a26d74
ejdataras <ejdaras@gmail.com>
2019-08-06 15:36:34 +0200

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # lire tout les taches
+  # get 'tasks' => 'tasks#index'
+  # # creer des taches
+  # get 'tasks/new' => 'tasks#new'
+  # post 'tasks' => 'tasks#create'
+  # # lire une tache
+  # get 'tasks/:id' => 'tasks#show'
+  # # updater une tache
+  # get 'tasks/:id/edit' => 'tasks#edit'
+  # patch 'tasks/:id' => 'tasks#update'
+  # # supprimer un tache
+  # delete 'tasks/:id' => 'tasks#destroy '
+  resources :tasks
 end
